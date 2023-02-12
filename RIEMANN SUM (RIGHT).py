51e6ba39ab17650a9c1d4c2ea4bb246051e30962
TheLudway <70478991+TheLudway@users.noreply.github.com>
2023-02-12 16:17:24 -0500

    TRADUCTION AND SIGNATURE

--- RIEMANN SUM (RIGHT).py
+++ RIEMANN SUM (RIGHT).py
@@ -1,33 +1,44 @@
 import fractions
 
-n = int(input("Ingrese el número de parejas ordenadas: " )) - 1 # Se ingresa la cantidad de n intervalos que se poseen
-b = float(input("Último límite de integración: " ))  # Se ingresa el número del último límite de integración (eje x normalmente)
-a = float(input("Primer límite de integración: " ))  # Se ingresa el númerl del primer límite de integración (eje x normalmente)
-delta_x = (b-a)/n  # Se cálcula delta de x
-diccionario = {}  # En este diccionario van a estar todas las parejas ordenadas
-Resultado = 0  # Se asigna un resultado con una variable para poder manipular a futuro
+n = int(input("Ingrese el número de parejas ordenadas: " )) - 1 # n number of couples
+b = float(input("Último límite de integración: " ))  # Last integration limit x axis
+a = float(input("Primer límite de integración: " ))  # first integration limit y axis
+delta_x = (b-a)/n  # Get Delta x
+diccionario = {}  # Dictionary where there are all the couples
+Resultado = 0  # Variable equal to 0 and in a future it can be manipulate
 
-for i in range(n+1):  # Se pide que se repita la acción de pedir datos n número de veces
-    x = input("Valor x de la pareja ordenada: " ) # Se pide la coordenada x
-    try:     #Verificación del x para ver si es una fracción o no
+for i in range(n+1):  
+"""Add to the dictionary all the couples"""
+    x = input("Valor x de la pareja ordenada: " ) # x coordenate
+    try:     # See if x is a fraction
         if not x.isnumeric():
             x = fractions.Fraction(x)
     except ValueError:
         (x + "NO SE ENCUENTRA EN EL DOMINIO")
     doubleofx = float(x) 
-    y = input("Valor y de la pareja ordenada: " ) # Se pide la coordenada y
-    try:    #Verificación del y para ver si es una fracción o no
+    y = input("Valor y de la pareja ordenada: " ) # y coordenate
+    try:    # See if y is a fraction
         if not y.isnumeric():
             y = fractions.Fraction(y)
     except ValueError:
         (y + "NO ES UN NÚMERO")
     doubleofy = float(y) 
-    diccionario.update({doubleofx:doubleofy}) # x and y se almacenan en el diccionario
+    diccionario.update({doubleofx:doubleofy}) # x and y get updated in the dictionary
 
-#OPERACIÓN PARA LA REGLA DEL TRAPECIO
+# Sum all the values and print the solution of the integral
 diccionario.pop(a)
 for i in diccionario.values():
     Resultado += i
 
 Resultado *= delta_x
 print(Resultado)
+
+"""
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣶⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⣄⣀⡀⣠⣾⡇⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀
+⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⢿⣿⣿⡇⠀⠀⠀⠀
+⠀⣶⣿⣦⣜⣿⣿⣿⡟⠻⣿⣿⣿⣿⣿⣿⣿⡿⢿⡏⣴⣺⣦⣙⣿⣷⣄⠀⠀⠀
+⠀⣯⡇⣻⣿⣿⣿⣿⣷⣾⣿⣬⣥⣭⣽⣿⣿⣧⣼⡇⣯⣇⣹⣿⣿⣿⣿⣧⠀⠀
+⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠸⣿⣿⣿⣿⣿⣿⣿⣷⠀
+"""
