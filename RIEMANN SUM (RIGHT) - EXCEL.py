c5ac865eecab9d7c12888fe63436292e57ba9532
TheLudway <70478991+TheLudway@users.noreply.github.com>
2023-02-12 15:32:25 -0500

    TRADUCTION AND SIGNATURE

--- RIEMANN SUM (RIGHT) - EXCEL.py
+++ RIEMANN SUM (RIGHT) - EXCEL.py
@@ -6,15 +6,24 @@ wb = load_workbook('SPEED.xlsx')
 ws = wb.active
 Resultado_suma = 0
 
-#RECORRIDO DE LAS CELDAS PARA LA MULTIPLICACIÓN POR (2) EN LA SUMA
+#Run over all the cells in the range and multiply each one by 2
 for row in range(3, 40982):
     for col in [2]:
         char = get_column_letter(col)
         Resultado_suma += (ws[char + str(row)].value) 
 
-#OBTENER LOS LÍMITES DE INTEGRACIÓN Y CÀLCULAR DELTA DE X
+#Get integration limits and determinate Delta x
 Delta_x = (ws["A40981"].value - ws["A2"].value) / 40980
 
 Resultado_final = (Resultado_suma * Delta_x)
 print(Resultado_final)
 
+"""
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣶⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⣄⣀⡀⣠⣾⡇⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀
+⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⢿⣿⣿⡇⠀⠀⠀⠀
+⠀⣶⣿⣦⣜⣿⣿⣿⡟⠻⣿⣿⣿⣿⣿⣿⣿⡿⢿⡏⣴⣺⣦⣙⣿⣷⣄⠀⠀⠀
+⠀⣯⡇⣻⣿⣿⣿⣿⣷⣾⣿⣬⣥⣭⣽⣿⣿⣧⣼⡇⣯⣇⣹⣿⣿⣿⣿⣧⠀⠀
+⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠸⣿⣿⣿⣿⣿⣿⣿⣷⠀
+"""
